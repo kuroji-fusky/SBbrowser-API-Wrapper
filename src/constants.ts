6405b5379c97a947e074f41925d29907811e4ed6
Kerby Keith Aquino <hello@kurojifusky.com>
2024-10-09 22:27:39 +0800

    add constants

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const SB_BASE_URL = "https://sb.ltn.fi";
+
+export const selectors = {
+  segmentTable: "table",
+  updatedTime: "div > div",
+} as const;
--- src/selectors.ts
+++
@@ -1,3 +0,0 @@
-const selectors = {} as const;
-
-export default selectors;
